e5ab2578b4e05f7ace49902cd184302de1b61bba
Prashant Gupta <prashant@quadrical.ai>
2024-07-05 20:30:19 +0530

    updates

---
+++ lua/plugins/lspconfig.lua
@@ -0,0 +1,46 @@
+return {}
+-- return {
+--   {
+--     "neovim/nvim-lspconfig",
+--     opts = {
+--       servers = {
+--         ruff_lsp = {
+--           mason = false,
+--           enabled = false,
+--         },
+--       },
+--     },
+--   },
+-- }
+--
+-- return {
+--   "nvim-lspconfig",
+--   opts = {
+--     ---@type lspconfig.options
+--     servers = {
+--       pyright = {
+--         capabilities = (function()
+--           local capabilities = vim.lsp.protocol.make_client_capabilities()
+--           capabilities.textDocument.publishDiagnostics.tagSupport.valueSet = { 2 }
+--           return capabilities
+--         end)(),
+--         settings = {
+--           python = {
+--             analysis = {
+--               useLibraryCodeForTypes = true,
+--               diagnosticSeverityOverrides = {
+--                 reportUnusedVariable = "warning", -- or anything
+--               },
+--               typeCheckingMode = "basic",
+--             },
+--           },
+--         },
+--       },
+--       ruff_lsp = {
+--         on_attach = function(client, _)
+--           client.server_capabilities.hoverProvider = false
+--         end,
+--       },
+--     },
+--   },
+-- }
